7609af4a111f3acbd04ca7cb53c6807fcd0f4094
baran beyazıt <=>
2022-01-05 04:14:04 +0300

    main

---
+++ tailwind-1/src/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+function burger(){
+
+    let fast = document.getElementById('burger_menu_ul');
+
+    if(fast.style.visibility == 'hidden'){
+        fast.style.visibility="visible";
+    }else{
+        fast.style.visibility='hidden'
+    }
+}
+
+function dropdown(){
+    let loww = document.getElementById('dropdown_items');
+
+    if(loww.style.visibility == 'hidden'){
+        loww.style.visibility="visible";
+    }else{
+        loww.style.visibility='hidden'
+    }
+}
+
+
+
+
