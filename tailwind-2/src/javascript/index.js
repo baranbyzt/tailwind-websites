95254e18387f3c5d8d03adfd0b9a5794a4937518
baran beyazıt <=>
2022-01-07 07:54:11 +0300

    main

---
+++ tailwind-2/src/javascript/index.js
@@ -0,0 +1,15 @@
+
+
+
+let menu_3 = document.getElementById("menu_ul_3");
+
+
+
+function burger_clicked(){
+
+  if(  menu_3.style.display=="none"){
+    menu_3.style.display="block"
+  }else{
+    menu_3.style.display="none"
+  }
+}
---
+++ tailwind-2/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/pages/*"],
+  theme: {
+    extend: {
+      colors: {
+        'header_1_color': '#645F88',
+        'header_4_color': 'rgba(45, 215, 238, 0.603)'
+      },
+    },
+  },
+  plugins: [],
+}
