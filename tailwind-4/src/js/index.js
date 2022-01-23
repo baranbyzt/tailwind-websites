2261f3d081960793ecd07697d7187645039a6f2b
baran beyazıt <byztmain2121@gmail.com>
2022-01-24 01:43:14 +0300

    tailwind-4 is added

---
+++ tailwind-4/src/js/index.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+let inner_ul_js =document.getElementById("inner_ul");
+
+    $(".burger_ittme").click(function(){
+       if(inner_ul_js.style.display == "none"){
+        inner_ul_js.style.display = "flex";
+       }
+       else{
+        inner_ul_js.style.display = "none";
+       }
+    });
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
